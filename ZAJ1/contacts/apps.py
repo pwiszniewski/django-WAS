d09509c8ad6bd347cac75a9ba08a9a0432827e0b
P W <przemo2048@gmail.com>
2019-11-26 23:08:54 +0100

    usuniecie niepotrzebnych folderów

--- ZAJ1/contacts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy
-
-class ContactsConfig(AppConfig):
-    name = 'contacts'
-    verbose_name = gettext_lazy('Contact app')
